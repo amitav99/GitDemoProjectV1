bc6362c6912aaba816dbff69b9c69fe258a94d1c
Amitav <amitavdebanth2000@gmail.com>
2024-01-29 00:32:19 +0530

    First Commit...

---
+++ src/com/demo/Demo.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+public class Demo {
+	public static void main(String args[]) {
+		String hungry= "Yes";
+		if (hungry=="Yes")
+			System.out.println("Eat Something.....");
+		else
+			System.out.println("Do some work!!!!!");
+			
+			
+	}
+
+}
